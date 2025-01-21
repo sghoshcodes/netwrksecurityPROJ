462f6b6c55d6267ca10f9c37694c728936234ad0
Shaunak Ghosh <shaunak.ghosh2016@gmail.com>
2025-01-21 16:57:00 -0500

    ETL Pipeline Launched

--- networksecurity/exception/exception.py
+++ networksecurity/exception/exception.py
@@ -10,7 +10,7 @@ class NetworkSecurityException(Exception):
 
     def __str__(self):
         return "Error occurred in python script name [{0}] line number [{1}] error message [{2}]".format(self.file_name, self.lineno, str(self.error_message))
-
+#testin'
 if __name__=='__main__':
     try:
         logger.logging.info("Enter the try block")
