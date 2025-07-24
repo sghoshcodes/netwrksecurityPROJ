698c3bb15ce3c61060cbee4561909e285c192436
Shaunak Ghosh <shaunak.ghosh2016@gmail.com>
2025-07-24 18:14:38 -0400

    data validation structure complete, w/p-value and file structure comparison

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import sys
 from networksecurity.components.data_ingestion import DataIngestion
+from networksecurity.components.data_validation import DataValidation
 from networksecurity.exception.exception import NetworkSecurityException
 from networksecurity.logging.logger import logging
-from networksecurity.entity.config_entity import DataIngestionConfig
+from networksecurity.entity.config_entity import DataIngestionConfig, DataValidationConfig
 from networksecurity.entity.config_entity import TrainingPipelineConfig
 
 if __name__ == '__main__':
@@ -13,6 +14,14 @@ if __name__ == '__main__':
         data_ingestion = DataIngestion(dataingestionconfig)
         logging.info("Initiate the data ingestion")
         dataingestionartifact = data_ingestion.initiate_data_ingestion()
+        logging.info("Data Initiation Completed")
         print(dataingestionartifact)
+        data_validation_config = DataValidationConfig(trainingpipelineconfig)
+        data_validation=DataValidation(dataingestionartifact, data_validation_config)
+        logging.info("Initiate the data Validation")
+        data_validation_artifact = data_validation.initiate_data_validation()
+        logging.info("data validation completed")
+        print(data_validation_artifact)
+        
     except Exception as e:
         raise NetworkSecurityException(e,sys)
