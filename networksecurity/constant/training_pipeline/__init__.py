d90b47a0e64ae82756c3817590b8c11caee2dcdc
Shaunak Ghosh <shaunak.ghosh2016@gmail.com>
2025-07-27 01:43:58 -0400

    all artifacts created, data ingestion, data validation, and data transformation

--- networksecurity/constant/training_pipeline/__init__.py
+++ networksecurity/constant/training_pipeline/__init__.py
@@ -25,3 +25,15 @@ DATA_VALIDATION_VALID_DIR: str = "validated"
 DATA_VALIDATION_INVALID_DIR: str = "invalid"
 DATA_VALIDATION_DRIFT_REPORT_DIR: str = "drift_report"
 DATA_VALIDATION_DRIFT_REPORT_FILE_NAME: str = "report.yaml"
+
+DATA_TRANSFORMATION_DIR_NAME: str = "data_transformation"
+DATA_TRANSFORMATION_TRANSFORMED_DATA_DIR: str = "transformed"
+DATA_TRANSFORMATION_TRANSFORMED_OBJECT_DIR: str = "transformed_object"
+PREPROCESSING_OBJECT_FILE_NAME:str = "preprocessing.pkl"
+
+##knn imputer to replace NaN values, it will see the 3 nearest neighbors, calculate av. value, replace the NaN value with that
+DATA_TRANSFORMATION_IMPUTER_PARAMS: dict = {
+    "missing_values": np.nan,
+    "n_neighbors": 3,
+    "weights": "uniform", 
+}
