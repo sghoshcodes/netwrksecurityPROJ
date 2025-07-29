e95c891cf0d12d5cdd0ec4364dbeb91c968a47b2
Shaunak Ghosh <shaunak.ghosh2016@gmail.com>
2025-07-29 02:51:21 -0400

    pipeline is finished, onto deployment

--- networksecurity/components/model_trainer.py
+++ networksecurity/components/model_trainer.py
@@ -24,6 +24,8 @@ from sklearn.ensemble import(
     RandomForestClassifier,
 )
 import mlflow
+import dagshub
+dagshub.init(repo_owner='sghoshcodes', repo_name='netwrksecurityPROJ', mlflow=True)
 
 class ModelTrainer:
     def __init__(self, model_trainer_config:ModelTrainerConfig, data_transformation_artifact:DataTransformationArtifact):
@@ -108,6 +110,8 @@ class ModelTrainer:
 
         Network_Model = NetworkModel(preprocessor=preprocessor, model = best_model)
         save_object(self.model_trainer_config.trained_model_file_path, obj=Network_Model)
+
+        save_object("final_model/model.pkl", best_model)
 
         ## Model Trainer Artifact
 
